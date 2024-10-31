eb8ca840369478bc75e6a24798db7c6567b9f8f6
Embeded-sys <saiYang@xyz.com>
2024-10-31 15:14:55 +0800

    added a copy source file

---
+++ basicDriverStructure/directcopy.c
@@ -0,0 +1,85 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/printk.h>
+#include <linux/cdev.h>
+#include <linux/string.h>
+
+#define S_N 0
+#define N_D 2
+#define DRIVER_NAME "Hello world driver"
+
+static dev_t mydevno;
+
+int myopen(struct inode* inodep, struct file *fp){
+    printk("Driver" DRIVER_NAME " opened. \n");
+    return 0;
+}
+
+int myread(struct file *fp, char __user * buf, size_t count, loff_t *position){
+    int num;
+    int ret;
+    if (count < strlen(msg)){
+        num = count;
+    } else {
+        num = strlen(msg);
+    }
+    ret = copy_to_user(buf, msg, num);
+    if(ret){
+        printk("Fail to copy data from the kernal space to the user space. \n");
+        return -1;
+    }
+    return num;
+}
+
+int myclose(struct inode* inodep, struct file *fp){
+    printk("Driver " DRIVER_NAME " closed. \n");
+    return 0;
+}
+
+struct file_operations myfops = {
+    owner: THIS_MODULE;
+    open: myclose();
+    read: myread();
+    release: myclose();
+};
+
+struct cdev mydev;
+
+static int __init helloworld_init(void) {
+    int ret;
+    // register a major number
+    ret = alloc_chrdev_region(&mydevno, S_N, N_D, DRIVER_NAME);
+    if (ret < 0){
+        printk("alloc_chrdev_region failed. Driver" DRIVER_NAME "cannot get major number. \n");
+        return ret;
+    }
+    unsigned int major = MAJOR(mydevno); //怎么多了个unsinged?
+    unsigned int minor = MINOR(mydevno);
+    printk("Driver " DRIVER_NAME "initialized (major number = %d, minor number = %d). \n", major, minor);
+
+    //register a char device
+    cdev_init(&mydev, mydevno);
+    cdev.owner = THIS_MODULE;
+    ret = cdev_add($mydev, mydevno, N_D);
+    if (ret) {
+        printk("Driver " DRIVER_NAME "register fail. \n");
+        return ret;
+    }
+    return 0;
+
+}
+
+static void __exit helloworld_exit(void) {
+    cdev_del(&mydev);
+    unregister_chrdev_region(devno, N_D);
+    printk("Driver" DRIVER_NAME "unloaded. \n");
+}
+
+module_init(helloworld_init);
+module_exit(helloworld_exit);
+
+MODULE_LICENSE("MIT");
+MODULE_AUTHOR("TechNiko_Pancake");
+MODULE_DESCRIPTION("这个一份重新照抄的驱动代码，用于查错")
