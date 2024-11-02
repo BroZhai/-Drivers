55b49f73ca73c0f72dbd29d9d72505a779fdf18d
Embeded-sys <saiYang@xyz.com>
2024-11-02 16:56:22 +0800

    彻底搞明白了mknod创建出来的是一个"硬件设备"，然后它并不会'主动'的和设备驱动文件去沟通(用户程序就来'唤醒'[模拟]硬件 与 驱动文件之间的通信)

--- basicDriverStructure/userHardware.c
+++ basicDriverStructure/userHardware.c
@@ -1,4 +1,6 @@
 // 这里去编写一个用户(开发板上的)程序，尝试去打开我们刚刚写的驱动
+// 实际上这个程序就是在"模拟硬件"，这个硬件将会从我们的 mknod 生成的 '硬件设备'中 去与 设备驱动文件 展开通信
+// (以上的理论共计花费了4天时间才他奶奶的搞明白，课讲的过于抽象和含糊了xwx)
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -12,16 +14,16 @@
 void main() {
   int fd;
   char buf[1024]; // 创建"用户接收缓冲区"
-  fd = open("dev/hellodriver", O_RDONLY);
+  fd = open("/userHard", O_RDONLY); // 这里就将会是我们打开的"mknod"生成的文件地址 (去你的 我直接存根目录了;|)
   if (fd<0){
     // 尝试open()驱动文件时出现了异常
-    perror("诶嘿，你猜我为什么显示了?");
+    perror("坏了，哥咱尽力了，但是可能没有找到对应的文件，出现了异常QAQ \n");
     exit(EXIT_FAILURE);
   }
   // 成功的打开了文件，开始读取内容
   int num = read(fd, buf, sizeof(buf)-1); // 这里-1的意义: 预留"最后一位"成0 当终止符
   buf[num] = 0; //设置读取内容后 的最后一位为0
-  printf("嘿！我们收到了来自/dev/hellodriver 的消息:\"%s\" \n", buf); //特意加了俩 " 作为修饰
+  printf("嘿！我们收到了来自/userHard 从 directcopy.ko 驱动文件中 读取 的消息:\"%s\" \n", buf); //特意加了俩 " 作为修饰
   close(fd);
   exit(EXIT_SUCCESS);
 }
